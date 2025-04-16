8fe10175876302d4a3df7f3c9ac7230bb4f2bf89
Isaac <trejomendoza.isaac0123@gmail.com>
2025-04-16 09:00:07 -0500

    added navbars and course page, updated UI in login, dashboard, and course.

--- src/app/components/dashboard-navbar/dashboard-navbar.component.ts
+++ src/app/components/dashboard-navbar/dashboard-navbar.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      dashboard-navbar works!
-    </p>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+  <div class="container d-flex justify-content-between align-items-center">
+    
+    <ul class="navbar-nav d-flex flex-row">
+
+      <li class="nav-item me-4">
+        <a class="nav-link">Grades</a>
+      </li>
+
+      <li class="nav-item me-4">
+        <a class="nav-link">Dashboard</a>
+      </li>
+
+      <li class="nav-item me-4">
+        <a class="nav-link" routerLink="/course">Course</a>
+      </li>
+
+    </ul>
+    
+  </div>
+</nav>
   `,
   styleUrl: './dashboard-navbar.component.scss'
 })
 export class DashboardNavbarComponent {
+  router: Router = inject(Router);
+  authService: AuthService = inject(AuthService);
 
+  signOut() {
+    return this.authService.signOut();
+  }
 }
---
+++ src/app/components/work-due/work-due.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-work-due',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div class="card dark-card p-4 my-3">
+      <h3 class="mb-3">Work Due</h3>
+      <p class="text-muted mb-0">
+        <!-- Future assingments info goes here -->
+         No assignments due yet.
+      </p>
+    </div>
+  `,
+  styleUrl: './work-due.component.scss'
+})
+export class WorkDueComponent {
+
+}
